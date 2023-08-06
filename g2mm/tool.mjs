3418a42b4eb537a142b8b8f8bf33cdf23b202c9e
pixel <pixel+github@chrissx.de>
2023-08-06 04:09:35 +0200

    g2mm 0.2.1
    add support for different styles to the cli tool

--- g2mm/tool.mjs
+++ g2mm/tool.mjs
@@ -1,7 +1,12 @@
 #!/usr/bin/env node
 import g2mm from "./index.mjs";
+import minimist from "minimist";
+
+const argv = minimist(process.argv.slice(2));
+const style = argv["genius"] || argv["g"] ? "genius"
+    : argv["plain"] || argv["p"] ? "plain" : "musixmatch";
 
 const input = [];
 // for some f-ing js-reason this cant be η-reduced
 process.stdin.on('data', (x) => input.push(x));
-process.stdin.on('end', () => console.log(g2mm(input.join(""))));
+process.stdin.on('end', () => console.log(g2mm(input.join(""), style)));
